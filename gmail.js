8849193e1dc65577518050ab008d47d927d10bda
wallondo <wallondolaila@gmail.com>
2024-12-13 09:19:01 +0000

    milisegundo

--- gmail.js
+++ gmail.js
@@ -10,7 +10,10 @@ const sender = nodemail.createTransport({
         user:"wallondolaila@gmail.com",
         //pass:"laila0101*"
         pass:"yiag baer tkdl ajco"
-    }
+    },
+    connectionTimeout: 300000,  // Ajuste o tempo de conexão
+    greetingTimeout: 300000,    // Ajuste o tempo de saudação
+    socketTimeout: 300000
 })
 
 const server = express();
@@ -27,38 +30,45 @@ server.post("/sms_gmail/:option",async(req,resp)=>{
     server.use(cors())
 
     if(req.params.option==1){
-        let tema = req.body.tema;
-        let cliente = req.body.cliente;
-        let email = req.body.email;
-        let conteudo = req.body.conteudo;
-        console.log(cliente)
-        await sender.sendMail({
-            // de momento tá fixo , depois vai ser dinamico , se aceitarem , mas eu vou faze-lo 
-            //só pra teste e agregação de conhecimento
-            from:"wallondolaila@gmail.com",
-            to:email,
-            subject:tema,
-            html:"<h4>"+conteudo+"</h4>"+"</br>"+`<a href=${cliente}> Esplorar </a>`, 
-            text:conteudo
-        }) 
-        //teste de acrescimo
-        .then((response)=>{
-        console.log("tudo fine aqui")
-        console.log(response)
+      async  function  send1(){
+            server.use(cors())
+            let tema = req.body.tema;
+            let cliente = req.body.cliente;
+            let email = req.body.email;
+            let conteudo = req.body.conteudo;
+            console.log(cliente)
+            await sender.sendMail({
+                // de momento tá fixo , depois vai ser dinamico , se aceitarem , mas eu vou faze-lo 
+                //só pra teste e agregação de conhecimento
+                from:"wallondolaila@gmail.com",
+                to:email,
+                subject:tema,
+                html:"<h4>"+conteudo+"</h4>"+"</br>"+`<a href=${cliente}> Esplorar </a>`, 
+                text:conteudo
+            }) 
+            //teste de acrescimo
+            .then((response)=>{
+            console.log("tudo fine aqui")
+            console.log(response)
 
-        return  resp.status(201).send({sms:"tudo feito",gmail:tema+"/"+cliente+"/"+conteudo})
-        })
-        .catch((err)=>{
-        console.log(err)
-        console.log("deu erro aqui")
+            return  resp.status(201).send({sms:"tudo feito",gmail:tema+"/"+cliente+"/"+conteudo})
+            })
+            .catch((err)=>{
+            console.log(err)
+            console.log("deu erro aqui")
 
-        return  resp.status(400).send("err o de envio")
-        })
+            return  resp.status(400).send("err o de envio")
+            })
+        }
+        send1()
     }else if(req.params.option==2){
+    server.use(cors())
+
         let emails = [...req.body]
         let num=0;
         var dune = null;
         var sended = [];
+      async  function send2(){
         console.log(emails)
             while(num<emails.length){
                 let tema = emails[num].tema;
@@ -94,6 +104,8 @@ server.post("/sms_gmail/:option",async(req,resp)=>{
             console.log("saiu")
                 return  resp.status(201).send({sms:"tudo feito",sended})
             }
+        }
+        send2()
        
     }
 
